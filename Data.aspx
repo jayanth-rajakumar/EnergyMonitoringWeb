846a74e013e66f579258d327ce75f79d4d374db1
jayanth-rajakumar <jayanth.rajakumar@gmail.com>
2018-04-17 20:52:11 +0530

    SQL Filtering

--- Data.aspx
+++ Data.aspx
@@ -17,7 +17,7 @@
    
       
         <asp:Panel ID="Panel1" runat="server" Width="703px" BorderStyle="Inset" BorderWidth="4px">
-              <asp:Label ID="Label1" runat="server" ForeColor="White" Text="Welcome, "></asp:Label><br /><br />
+              <asp:Label ID="Label1" runat="server" ForeColor="White" Text="MAC ID:"></asp:Label><br /><br />
         <asp:Label ID="Label2" runat="server" Text="Choose a parameter:" ForeColor="White"></asp:Label><br />
         <asp:DropDownList ID="DropDownList1" runat="server">
             <asp:ListItem Selected="True">Energy</asp:ListItem>
@@ -27,18 +27,231 @@
         </asp:DropDownList>  <asp:TextBox ID="TextBox2" runat="server" Width="105px">From</asp:TextBox>
                <asp:TextBox ID="TextBox3" runat="server" Width="98px">To</asp:TextBox>&nbsp;
 &nbsp;<asp:Button ID="Button1" runat="server" Text="View Readings" Width="136px" OnClick="Button1_Click" />
+              &nbsp;
+&nbsp;<asp:Button ID="Refresh" runat="server" Text="Refresh" />
         <br /><br />
              <asp:DropDownList ID="DD_Year" runat="server">
+                 <asp:ListItem>2018</asp:ListItem>
+                 <asp:ListItem>2017</asp:ListItem>
+                 <asp:ListItem>2016</asp:ListItem>
+                 <asp:ListItem>2015</asp:ListItem>
+                 <asp:ListItem>2014</asp:ListItem>
+                 <asp:ListItem>2013</asp:ListItem>
+                 <asp:ListItem>2012</asp:ListItem>
+                 <asp:ListItem>2011</asp:ListItem>
+                 <asp:ListItem>2010</asp:ListItem>
+                 <asp:ListItem>2009</asp:ListItem>
+                 <asp:ListItem>2008</asp:ListItem>
+                 <asp:ListItem>2007</asp:ListItem>
+                 <asp:ListItem>2006</asp:ListItem>
+                 <asp:ListItem>2005</asp:ListItem>
+                 <asp:ListItem>2004</asp:ListItem>
+                 <asp:ListItem>2003</asp:ListItem>
+                 <asp:ListItem>2002</asp:ListItem>
+                 <asp:ListItem>2001</asp:ListItem>
+                 <asp:ListItem>2000</asp:ListItem>
+                 <asp:ListItem>1999</asp:ListItem>
+                 <asp:ListItem>1998</asp:ListItem>
+                 <asp:ListItem>1997</asp:ListItem>
     </asp:DropDownList> <asp:Label ID="Label3" runat="server" ForeColor="White" Text=" / "/>
     <asp:DropDownList ID="DD_Month" runat="server">
+        <asp:ListItem>12</asp:ListItem>
+        <asp:ListItem>11</asp:ListItem>
+        <asp:ListItem>10</asp:ListItem>
+        <asp:ListItem>9</asp:ListItem>
+        <asp:ListItem>8</asp:ListItem>
+        <asp:ListItem>7</asp:ListItem>
+        <asp:ListItem>6</asp:ListItem>
+        <asp:ListItem>5</asp:ListItem>
+        <asp:ListItem>4</asp:ListItem>
+        <asp:ListItem>3</asp:ListItem>
+        <asp:ListItem>2</asp:ListItem>
+        <asp:ListItem>1</asp:ListItem>
     </asp:DropDownList><asp:Label ID="Label4" runat="server" ForeColor="White" Text=" / "/>
     <asp:DropDownList ID="DD_Date" runat="server">
+
+        <asp:ListItem>31</asp:ListItem>
+<asp:ListItem>30</asp:ListItem>
+<asp:ListItem>29</asp:ListItem>
+<asp:ListItem>28</asp:ListItem>
+<asp:ListItem>27</asp:ListItem>
+<asp:ListItem>26</asp:ListItem>
+<asp:ListItem>25</asp:ListItem>
+<asp:ListItem>24</asp:ListItem>
+<asp:ListItem>23</asp:ListItem>
+<asp:ListItem>22</asp:ListItem>
+<asp:ListItem>21</asp:ListItem>
+<asp:ListItem>20</asp:ListItem>
+<asp:ListItem>19</asp:ListItem>
+<asp:ListItem>18</asp:ListItem>
+<asp:ListItem>17</asp:ListItem>
+<asp:ListItem>16</asp:ListItem>
+<asp:ListItem>15</asp:ListItem>
+<asp:ListItem>14</asp:ListItem>
+<asp:ListItem>13</asp:ListItem>
+<asp:ListItem>12</asp:ListItem>
+<asp:ListItem>11</asp:ListItem>
+<asp:ListItem>10</asp:ListItem>
+<asp:ListItem>9</asp:ListItem>
+<asp:ListItem>8</asp:ListItem>
+<asp:ListItem>7</asp:ListItem>
+<asp:ListItem>6</asp:ListItem>
+<asp:ListItem>5</asp:ListItem>
+<asp:ListItem>4</asp:ListItem>
+<asp:ListItem>3</asp:ListItem>
+<asp:ListItem>2</asp:ListItem>
+<asp:ListItem>1</asp:ListItem>
     </asp:DropDownList><asp:Label ID="Label5" runat="server" ForeColor="White" Text=" , "/>
     <asp:DropDownList ID="DD_Hours" runat="server">
+        <asp:ListItem>0</asp:ListItem>
+<asp:ListItem>1</asp:ListItem>
+<asp:ListItem>2</asp:ListItem>
+<asp:ListItem>3</asp:ListItem>
+<asp:ListItem>4</asp:ListItem>
+<asp:ListItem>5</asp:ListItem>
+<asp:ListItem>6</asp:ListItem>
+<asp:ListItem>7</asp:ListItem>
+<asp:ListItem>8</asp:ListItem>
+<asp:ListItem>9</asp:ListItem>
+<asp:ListItem>10</asp:ListItem>
+<asp:ListItem>11</asp:ListItem>
+<asp:ListItem>12</asp:ListItem>
+<asp:ListItem>13</asp:ListItem>
+<asp:ListItem>14</asp:ListItem>
+<asp:ListItem>15</asp:ListItem>
+<asp:ListItem>16</asp:ListItem>
+<asp:ListItem>17</asp:ListItem>
+<asp:ListItem>18</asp:ListItem>
+<asp:ListItem>19</asp:ListItem>
+<asp:ListItem>20</asp:ListItem>
+<asp:ListItem>21</asp:ListItem>
+<asp:ListItem>22</asp:ListItem>
+<asp:ListItem>23</asp:ListItem>
+
     </asp:DropDownList><asp:Label ID="Label6" runat="server" ForeColor="White" Text=" : "/>
     <asp:DropDownList ID="DD_Minutes" runat="server">
+        <asp:ListItem>0</asp:ListItem>
+<asp:ListItem>1</asp:ListItem>
+<asp:ListItem>2</asp:ListItem>
+<asp:ListItem>3</asp:ListItem>
+<asp:ListItem>4</asp:ListItem>
+<asp:ListItem>5</asp:ListItem>
+<asp:ListItem>6</asp:ListItem>
+<asp:ListItem>7</asp:ListItem>
+<asp:ListItem>8</asp:ListItem>
+<asp:ListItem>9</asp:ListItem>
+<asp:ListItem>10</asp:ListItem>
+<asp:ListItem>11</asp:ListItem>
+<asp:ListItem>12</asp:ListItem>
+<asp:ListItem>13</asp:ListItem>
+<asp:ListItem>14</asp:ListItem>
+<asp:ListItem>15</asp:ListItem>
+<asp:ListItem>16</asp:ListItem>
+<asp:ListItem>17</asp:ListItem>
+<asp:ListItem>18</asp:ListItem>
+<asp:ListItem>19</asp:ListItem>
+<asp:ListItem>20</asp:ListItem>
+<asp:ListItem>21</asp:ListItem>
+<asp:ListItem>22</asp:ListItem>
+<asp:ListItem>23</asp:ListItem>
+<asp:ListItem>24</asp:ListItem>
+<asp:ListItem>25</asp:ListItem>
+<asp:ListItem>26</asp:ListItem>
+<asp:ListItem>27</asp:ListItem>
+<asp:ListItem>28</asp:ListItem>
+<asp:ListItem>29</asp:ListItem>
+<asp:ListItem>30</asp:ListItem>
+<asp:ListItem>31</asp:ListItem>
+<asp:ListItem>32</asp:ListItem>
+<asp:ListItem>33</asp:ListItem>
+<asp:ListItem>34</asp:ListItem>
+<asp:ListItem>35</asp:ListItem>
+<asp:ListItem>36</asp:ListItem>
+<asp:ListItem>37</asp:ListItem>
+<asp:ListItem>38</asp:ListItem>
+<asp:ListItem>39</asp:ListItem>
+<asp:ListItem>40</asp:ListItem>
+<asp:ListItem>41</asp:ListItem>
+<asp:ListItem>42</asp:ListItem>
+<asp:ListItem>43</asp:ListItem>
+<asp:ListItem>44</asp:ListItem>
+<asp:ListItem>45</asp:ListItem>
+<asp:ListItem>46</asp:ListItem>
+<asp:ListItem>47</asp:ListItem>
+<asp:ListItem>48</asp:ListItem>
+<asp:ListItem>49</asp:ListItem>
+<asp:ListItem>50</asp:ListItem>
+<asp:ListItem>51</asp:ListItem>
+<asp:ListItem>52</asp:ListItem>
+<asp:ListItem>53</asp:ListItem>
+<asp:ListItem>54</asp:ListItem>
+<asp:ListItem>55</asp:ListItem>
+<asp:ListItem>56</asp:ListItem>
+<asp:ListItem>57</asp:ListItem>
+<asp:ListItem>58</asp:ListItem>
+<asp:ListItem>59</asp:ListItem>
     </asp:DropDownList><asp:Label ID="Label7" runat="server" ForeColor="White" Text=" : "/>
     <asp:DropDownList ID="DD_Seconds" runat="server">
+        <asp:ListItem>0</asp:ListItem>
+<asp:ListItem>1</asp:ListItem>
+<asp:ListItem>2</asp:ListItem>
+<asp:ListItem>3</asp:ListItem>
+<asp:ListItem>4</asp:ListItem>
+<asp:ListItem>5</asp:ListItem>
+<asp:ListItem>6</asp:ListItem>
+<asp:ListItem>7</asp:ListItem>
+<asp:ListItem>8</asp:ListItem>
+<asp:ListItem>9</asp:ListItem>
+<asp:ListItem>10</asp:ListItem>
+<asp:ListItem>11</asp:ListItem>
+<asp:ListItem>12</asp:ListItem>
+<asp:ListItem>13</asp:ListItem>
+<asp:ListItem>14</asp:ListItem>
+<asp:ListItem>15</asp:ListItem>
+<asp:ListItem>16</asp:ListItem>
+<asp:ListItem>17</asp:ListItem>
+<asp:ListItem>18</asp:ListItem>
+<asp:ListItem>19</asp:ListItem>
+<asp:ListItem>20</asp:ListItem>
+<asp:ListItem>21</asp:ListItem>
+<asp:ListItem>22</asp:ListItem>
+<asp:ListItem>23</asp:ListItem>
+<asp:ListItem>24</asp:ListItem>
+<asp:ListItem>25</asp:ListItem>
+<asp:ListItem>26</asp:ListItem>
+<asp:ListItem>27</asp:ListItem>
+<asp:ListItem>28</asp:ListItem>
+<asp:ListItem>29</asp:ListItem>
+<asp:ListItem>30</asp:ListItem>
+<asp:ListItem>31</asp:ListItem>
+<asp:ListItem>32</asp:ListItem>
+<asp:ListItem>33</asp:ListItem>
+<asp:ListItem>34</asp:ListItem>
+<asp:ListItem>35</asp:ListItem>
+<asp:ListItem>36</asp:ListItem>
+<asp:ListItem>37</asp:ListItem>
+<asp:ListItem>38</asp:ListItem>
+<asp:ListItem>39</asp:ListItem>
+<asp:ListItem>40</asp:ListItem>
+<asp:ListItem>41</asp:ListItem>
+<asp:ListItem>42</asp:ListItem>
+<asp:ListItem>43</asp:ListItem>
+<asp:ListItem>44</asp:ListItem>
+<asp:ListItem>45</asp:ListItem>
+<asp:ListItem>46</asp:ListItem>
+<asp:ListItem>47</asp:ListItem>
+<asp:ListItem>48</asp:ListItem>
+<asp:ListItem>49</asp:ListItem>
+<asp:ListItem>50</asp:ListItem>
+<asp:ListItem>51</asp:ListItem>
+<asp:ListItem>52</asp:ListItem>
+<asp:ListItem>53</asp:ListItem>
+<asp:ListItem>54</asp:ListItem>
+<asp:ListItem>55</asp:ListItem>
+<asp:ListItem>56</asp:ListItem>
+<asp:ListItem>57</asp:ListItem>
+<asp:ListItem>58</asp:ListItem>
+<asp:ListItem>59</asp:ListItem>
     </asp:DropDownList>
 
               <asp:Button ID="Button2" runat="server" Text="From" />
--- Data.aspx.vb
+++ Data.aspx.vb
@@ -9,11 +9,197 @@ Partial Class Data
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        ' Session("username_sval") = "5CCF7FB0F47F"
+        Session("username_sval") = "5CCF7FB0F47F"
         Label1.Text = "MAC ID: " & Session("username_sval")
 
 
 
+    
+
+    End Sub
+  
+
+    Sub cleartable(a As String)
+        Dim con = New OleDbConnection("PROVIDER=Microsoft.Jet.OLEDB.4.0;" + "DATA SOURCE=" + Server.MapPath("Users.mdb"))
+        Dim cmd As OleDbCommand
+        cmd = New OleDbCommand()
+        cmd.CommandText = "delete from " + a
+
+        cmd.Connection = con
+        con.Open()
+        cmd.ExecuteNonQuery()
+        con.Close()
+
+    End Sub
+    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox2.Text = "From" And TextBox3.Text = "To" Then
+            Dim con = New OleDbConnection("PROVIDER=Microsoft.Jet.OLEDB.4.0;" + "DATA SOURCE=" + Server.MapPath("Users.mdb"))
+            Dim sz As String = DropDownList1.SelectedItem.Text
+            If sz = "Energy" Then
+                sz = "ActivePower"
+            End If
+            Dim query As String = "SELECT * from " & sz & " where User='" & Session("username_sval") & "'"
+            Dim oledbAdapter As OleDbDataAdapter
+            Dim ds As New DataSet
+            con.Open()
+            oledbAdapter = New OleDbDataAdapter(query, con)
+            Try
+                oledbAdapter.Fill(ds)
+            Catch ex As Exception
+                MsgBox(query)
+            End Try
+            For k As Integer = 1 To Table1.Rows.Count - 1
+                Table1.Rows.RemoveAt(1)
+            Next
+
+            For i = 0 To ds.Tables(0).Rows.Count - 1
+
+
+                Dim nDateTime As System.DateTime = New System.DateTime(1970, 1, 1, 0, 0, 0, 0)
+                Dim nTimestamp As Double = Double.Parse(ds.Tables(0).Rows(i).Item(1))
+                nDateTime = nDateTime.AddSeconds(nTimestamp)
+                nDateTime = nDateTime.AddSeconds(330 * 60)
+
+
+                Dim tRow As TableRow = New TableRow()
+                tRow.ForeColor = Drawing.Color.White
+                Dim cell1, cell2 As New TableCell
+                cell1.Text = nDateTime
+
+                cell2.Text = ds.Tables(0).Rows(i).Item(2)
+                tRow.Cells.Add(cell1)
+                tRow.Cells.Add(cell2)
+                Table1.Rows.Add(tRow)
+
+
+
+            Next
+
+            Table1.Visible = True
+
+
+            Chart1.Series.Clear()
+            Chart1.Titles.Add(sz)
+            Dim s As New Series
+            s.ChartType = SeriesChartType.Line
+
+            For k As Integer = 1 To Table1.Rows.Count - 1
+
+                s.Points.AddXY(Table1.Rows(k).Cells(0).Text, Table1.Rows(k).Cells(1).Text)
+            Next
+            Chart1.Series.Add(s)
+            Chart1.Visible = True
+        Else
+            Dim s1() As String = TextBox2.Text.Split("/")
+            Dim s2() As String = TextBox3.Text.Split("/")
+
+            Dim s1DateTime As System.DateTime = New System.DateTime(Integer.Parse(s1(0)), Integer.Parse(s1(1)), Integer.Parse(s1(2)), Integer.Parse(s1(3)), Integer.Parse(s1(4)), Integer.Parse(s1(5)))
+            Dim s2DateTime As System.DateTime = New System.DateTime(Integer.Parse(s2(0)), Integer.Parse(s2(1)), Integer.Parse(s2(2)), Integer.Parse(s2(3)), Integer.Parse(s2(4)), Integer.Parse(s2(5)))
+
+            Dim refDateTime As System.DateTime = New System.DateTime(1970, 1, 1, 0, 0, 0, 0)
+
+            Dim unixTimestamp1 = (s1DateTime.Subtract(New DateTime(1970, 1, 1))).TotalSeconds - 330 * 60
+            Dim unixTimestamp2 = (s2DateTime.Subtract(New DateTime(1970, 1, 1))).TotalSeconds - 330 * 60
+
+            MsgBox(unixTimestamp1)
+            MsgBox(unixTimestamp2)
+
+            Dim con = New OleDbConnection("PROVIDER=Microsoft.Jet.OLEDB.4.0;" + "DATA SOURCE=" + Server.MapPath("Users.mdb"))
+            Dim sz As String = DropDownList1.SelectedItem.Text
+            If sz = "Energy" Then
+                sz = "ActivePower"
+            End If
+            Dim query As String = "SELECT * from " & sz & " where User='" & Session("username_sval") & "' and Timestamp>" & unixTimestamp1 & " and Timestamp<" & unixTimestamp2
+            'MsgBox(query)
+
+            Dim oledbAdapter As OleDbDataAdapter
+            Dim ds As New DataSet
+            con.Open()
+            oledbAdapter = New OleDbDataAdapter(query, con)
+            Try
+                oledbAdapter.Fill(ds)
+            Catch ex As Exception
+                MsgBox(query)
+            End Try
+            For k As Integer = 1 To Table1.Rows.Count - 1
+                Table1.Rows.RemoveAt(1)
+            Next
+
+
+            For i = 0 To ds.Tables(0).Rows.Count - 1
+
+
+                Dim nDateTime As System.DateTime = New System.DateTime(1970, 1, 1, 0, 0, 0, 0)
+                Dim nTimestamp As Double = Double.Parse(ds.Tables(0).Rows(i).Item(1))
+
+                nDateTime = nDateTime.AddSeconds(nTimestamp)
+                nDateTime = nDateTime.AddSeconds(330 * 60)
+
+                Dim tRow As TableRow = New TableRow()
+                tRow.ForeColor = Drawing.Color.White
+                Dim cell1, cell2 As New TableCell
+                cell1.Text = nDateTime
+
+
+                cell2.Text = ds.Tables(0).Rows(i).Item(2)
+                tRow.Cells.Add(cell1)
+                tRow.Cells.Add(cell2)
+                Table1.Rows.Add(tRow)
+
+
+
+
+            Next
+
+
+            Table1.Visible = True
+
+
+            Chart1.Series.Clear()
+            Chart1.Titles.Add(sz)
+            Dim s As New Series
+            s.ChartType = SeriesChartType.Line
+
+            For k As Integer = 1 To Table1.Rows.Count - 1
+
+                s.Points.AddXY(Table1.Rows(k).Cells(0).Text, Table1.Rows(k).Cells(1).Text)
+            Next
+            Chart1.Series.Add(s)
+            Chart1.Visible = True
+
+        End If
+
+
+
+
+
+
+
+    End Sub
+
+    Protected Sub TextBox2_TextChanged(sender As Object, e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+   
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim strx As String = DD_Year.SelectedItem.Text & "/" & DD_Month.SelectedItem.Text & "/" & DD_Date.SelectedItem.Text & "/" & DD_Hours.SelectedItem.Text & "/" & DD_Minutes.SelectedItem.Text & "/" & DD_Seconds.SelectedItem.Text
+        TextBox2.Text = strx
+
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim strx As String = DD_Year.SelectedItem.Text & "/" & DD_Month.SelectedItem.Text & "/" & DD_Date.SelectedItem.Text & "/" & DD_Hours.SelectedItem.Text & "/" & DD_Minutes.SelectedItem.Text & "/" & DD_Seconds.SelectedItem.Text
+        TextBox3.Text = strx
+    End Sub
+
+    Protected Sub Refresh_Click(sender As Object, e As EventArgs) Handles Refresh.Click
         If IO.File.Exists(Server.MapPath("firebasedata.json")) Then
 
             IO.File.Delete(Server.MapPath("firebasedata.json"))
@@ -25,7 +211,7 @@ Partial Class Data
         webc.Dispose()
 
 
-        Dim readText As String = File.ReadAllText(Server.MapPath("firebasedata.json"))
+
         cleartable("ApparentPower")
         cleartable("ActivePower")
         cleartable("ReactivePower")
@@ -34,6 +220,8 @@ Partial Class Data
         cleartable("IRMS")
         cleartable("Frequency")
 
+
+        Dim readText As String = File.ReadAllText(Server.MapPath("firebasedata.json"))
         Dim json As JObject = JObject.Parse(readText)
 
         For Each Row In json(Session("username_sval")).ToList()
@@ -68,150 +256,5 @@ Partial Class Data
 skip:
 
         Next
-
-        DD_Year.Items.Clear()
-        DD_Month.Items.Clear()
-        DD_Date.Items.Clear()
-        DD_Hours.Items.Clear()
-        DD_Minutes.Items.Clear()
-        DD_Seconds.Items.Clear()
-
-        For i = 2018 To 1970 Step -1
-            DD_Year.Items.Add(i)
-        Next
-
-        For i = 1 To 12
-            DD_Month.Items.Add(i)
-        Next
-
-        For i = 1 To 31
-            DD_Date.Items.Add(i)
-        Next
-
-        For i = 0 To 23
-            DD_Hours.Items.Add(i)
-        Next
-        For i = 0 To 59
-            DD_Minutes.Items.Add(i)
-        Next
-        For i = 0 To 59
-            DD_Seconds.Items.Add(i)
-        Next
-
-    End Sub
-  
-
-    Sub cleartable(a As String)
-        Dim con = New OleDbConnection("PROVIDER=Microsoft.Jet.OLEDB.4.0;" + "DATA SOURCE=" + Server.MapPath("Users.mdb"))
-        Dim cmd As OleDbCommand
-        cmd = New OleDbCommand()
-        cmd.CommandText = "delete from " + a
-
-        cmd.Connection = con
-        con.Open()
-        cmd.ExecuteNonQuery()
-        con.Close()
-
-    End Sub
-    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
-
-    End Sub
-
-    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        Dim con = New OleDbConnection("PROVIDER=Microsoft.Jet.OLEDB.4.0;" + "DATA SOURCE=" + Server.MapPath("Users.mdb"))
-        Dim sz As String = DropDownList1.SelectedItem.Text
-        If sz = "Energy" Then
-            sz = "ActivePower"
-        End If
-        Dim query As String = "SELECT * from " & sz & " where User='" & Session("username_sval") & "'"
-        Dim oledbAdapter As OleDbDataAdapter
-        Dim ds As New DataSet
-        con.Open()
-        oledbAdapter = New OleDbDataAdapter(query, con)
-        Try
-            oledbAdapter.Fill(ds)
-        Catch ex As Exception
-            MsgBox(query)
-        End Try
-        For k As Integer = 1 To Table1.Rows.Count - 1
-            Table1.Rows.RemoveAt(1)
-        Next
-
-        For i = 0 To ds.Tables(0).Rows.Count - 1
-
-
-            Dim nDateTime As System.DateTime = New System.DateTime(1970, 1, 1, 0, 0, 0, 0)
-            Dim nTimestamp As Double = Double.Parse(ds.Tables(0).Rows(i).Item(1))
-            nDateTime = nDateTime.AddSeconds(nTimestamp)
-            nDateTime = nDateTime.AddSeconds(330 * 60)
-
-            If TextBox2.Text = "From" And TextBox3.Text = "To" Then
-                Dim tRow As TableRow = New TableRow()
-                tRow.ForeColor = Drawing.Color.White
-                Dim cell1, cell2 As New TableCell
-                cell1.Text = nDateTime
-
-                cell2.Text = ds.Tables(0).Rows(i).Item(2)
-                tRow.Cells.Add(cell1)
-                tRow.Cells.Add(cell2)
-                Table1.Rows.Add(tRow)
-            Else
-                Dim s1() As String = TextBox2.Text.Split("/")
-                Dim s2() As String = TextBox3.Text.Split("/")
-
-                Dim s1DateTime As System.DateTime = New System.DateTime(Integer.Parse(s1(0)), Integer.Parse(s1(1)), Integer.Parse(s1(2)), Integer.Parse(s1(3)), Integer.Parse(s1(4)), Integer.Parse(s1(5)))
-                Dim s2DateTime As System.DateTime = New System.DateTime(Integer.Parse(s2(0)), Integer.Parse(s2(1)), Integer.Parse(s2(2)), Integer.Parse(s2(3)), Integer.Parse(s2(4)), Integer.Parse(s2(5)))
-                'MsgBox(s1DateTime.ToString)
-                'MsgBox(s2DateTime.ToString)
-
-                If nDateTime >= s1DateTime And nDateTime <= s2DateTime Then
-                    Dim tRow As TableRow = New TableRow()
-                    tRow.ForeColor = Drawing.Color.White
-                    Dim cell1, cell2 As New TableCell
-                    cell1.Text = nDateTime
-
-                    cell2.Text = ds.Tables(0).Rows(i).Item(2)
-                    tRow.Cells.Add(cell1)
-                    tRow.Cells.Add(cell2)
-                    Table1.Rows.Add(tRow)
-                End If
-            End If
-
-         
-        Next
-
-        Table1.Visible = True
-
-
-        Chart1.Series.Clear()
-        Chart1.Titles.Add(sz)
-        Dim s As New Series
-        s.ChartType = SeriesChartType.Line
-
-        For k As Integer = 1 To Table1.Rows.Count - 1
-
-            s.Points.AddXY(Table1.Rows(k).Cells(0).Text, Table1.Rows(k).Cells(1).Text)
-        Next
-        Chart1.Series.Add(s)
-        Chart1.Visible = True
-
-    End Sub
-
-    Protected Sub TextBox2_TextChanged(sender As Object, e As System.EventArgs) Handles TextBox2.TextChanged
-
-    End Sub
-
-   
-
-    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim strx As String = DD_Year.SelectedItem.Text & "/" & DD_Month.SelectedItem.Text & "/" & DD_Date.SelectedItem.Text & "/" & DD_Hours.SelectedItem.Text & "/" & DD_Minutes.SelectedItem.Text & "/" & DD_Seconds.SelectedItem.Text
-        TextBox2.Text = strx
-
-    End Sub
-
-    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Dim strx As String = DD_Year.SelectedItem.Text & "/" & DD_Month.SelectedItem.Text & "/" & DD_Date.SelectedItem.Text & "/" & DD_Hours.SelectedItem.Text & "/" & DD_Minutes.SelectedItem.Text & "/" & DD_Seconds.SelectedItem.Text
-        TextBox3.Text = strx
     End Sub
 End Class
